0257301ed284f9392ebdbf2ec92ba2a247b90d6a
Lilian Patino Ortiz <lilian@Lilians-MacBook-Pro.local>
2023-07-24 21:06:45 -0700

    Testing the final api selected

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,24 @@
 let weatherAPIKey = "6f71590911e8c3802b29fe6c49229551"; // feel free to put yours!
 let jobsAPIKey = "bcb287f285mshb6ac2f0478c16f0p127325jsn154ee8e2efdc"; // feel free to put yours!
-var cityName = "Austin"; // to be input by the user
-var countryCode = "US"; // fixed value, we only want to search for cities in the US
+var countryName = "US";
+var cityName = "Texas"; // to be input by the user
+var query = "Python developer"; // to be input by the user
 
 async function jobsApiCall(cityState) {
   // 3. Api call to get jobs in the given location
-  const url = "https://linkedin-jobs-search.p.rapidapi.com/";
+  const url =
+    "https://jsearch.p.rapidapi.com/search?query=" +
+    query +
+    "in%20" +
+    cityName +
+    "%2C%20" +
+    countryName +
+    "&page=1&num_pages=1";
   const options = {
-    method: "POST",
+    method: "GET",
     headers: {
-      "content-type": "application/json",
       "X-RapidAPI-Key": jobsAPIKey,
-      "X-RapidAPI-Host": "linkedin-jobs-search.p.rapidapi.com",
-    },
-    body: {
-      search_terms: "javascript programmer",
-      location: "Chicago, IL",
-      page: "1",
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
     },
   };
 
@@ -55,7 +57,7 @@ async function cityApiCall() {
     "http://api.openweathermap.org/geo/1.0/direct?q=" +
     cityName +
     "," +
-    countryCode +
+    countryName +
     "&limit=10&appid=" +
     weatherAPIKey;
 
