2dea383d2a841279b0b71566ac664ec56c07f460
Lilian Patino Ortiz <lilian@Lilians-MacBook-Pro.local>
2023-07-27 20:35:31 -0700

    Sending the values from initial page to index page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,19 @@
 let weatherAPIKey = "6f71590911e8c3802b29fe6c49229551"; // feel free to put yours!
 let jobsAPIKey = "bcb287f285mshb6ac2f0478c16f0p127325jsn154ee8e2efdc"; // feel free to put yours!
-var countryName = "US";
-var cityName = "Texas"; // to be input by the user
-var query = "Python developer"; // to be input by the user
+var searchButtonElement = document.querySelector(".button");
+var keywordElement = document.querySelector("#keyword-input");
+var locationElement = document.querySelector("#location-input");
+
+var keywordValue;
+var locationValue;
 
 async function jobsApiCall(cityState) {
   // 3. Api call to get jobs in the given location
   const url =
     "https://jsearch.p.rapidapi.com/search?query=" +
-    query +
+    keywordValue +
     "in%20" +
-    cityName +
-    "%2C%20" +
-    countryName +
+    locationValue +
     "&page=1&num_pages=1";
   const options = {
     method: "GET",
@@ -45,7 +46,7 @@ async function weatherApiCall(cityLat, cityLon, cityState) {
     const data = await response.json();
     console.log("weatherApiCall --------");
     console.log(data);
-    jobsApiCall(cityState);
+    //jobsApiCall(cityState);
   } catch (error) {
     console.error(error);
   }
@@ -55,9 +56,7 @@ async function cityApiCall() {
   // 1. API call to get lat & lon from the city selected
   var url =
     "http://api.openweathermap.org/geo/1.0/direct?q=" +
-    cityName +
-    "," +
-    countryName +
+    locationValue +
     "&limit=10&appid=" +
     weatherAPIKey;
 
@@ -74,5 +73,34 @@ async function cityApiCall() {
     console.error(error);
   }
 }
+function getParams() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const keywordString = urlParams.get("keyword");
+  const locationString = urlParams.get("location");
+  console.log(keywordString);
+  console.log(locationString);
+  keywordElement.value = keywordString;
+  keywordValue = keywordString;
+  locationElement.value = locationString;
+  locationValue = locationString;
+  cityApiCall();
+}
 
-//cityApiCall();
+var searchFormEl = document.querySelector("#search-form");
+
+function handleSearchFormSubmit(event) {
+  event.preventDefault();
+  keywordValue = document.querySelector("#keyword-input").value;
+  locationValue = document.querySelector("#location-input").value;
+  if (!keywordValue || !locationValue) {
+    console.error("You need a search input value!");
+    return;
+  }
+  var locQueryUrl =
+    "./index.html?keyword=" + keywordValue + "&location=" + locationValue;
+  document.location.href = locQueryUrl;
+}
+
+searchButtonElement.addEventListener("submit", handleSearchFormSubmit);
+
+getParams();
